94a466fbc3008c34aebead96ee309b921071b771
alexwhin <alexwhin@gmail.com>
2017-07-23 13:36:22 +0100

    Small ES6 fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-var path = require('path')
+var path = require('path');
 module.exports = {
   entry: './src/ImagePlaceholder.js',
   output: {
@@ -24,4 +24,4 @@ module.exports = {
   externals: {
     'react': 'commonjs react'
   }
-}
+};
